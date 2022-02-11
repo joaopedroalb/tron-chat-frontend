a4551009f241e22684c5f4bcfae9e6c0cdc893db
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-02-11 16:28:13 -0300

    Add style in login component

--- src/components/LoginWallet/index.tsx
+++ src/components/LoginWallet/index.tsx
@@ -8,8 +8,17 @@ type LoginWalletProps = {
 export default function LoginWallet(props:LoginWalletProps) {
   return (
     <Box>
+      <label>Conecte-se usando a Tron Link</label>
       <button onClick={props.handleClick}>Logar</button>
-      {props.loaded&&<p>Por favor verifique se esta com a extensão do tron link</p>}
+      {props.loaded&&
+        (
+          <p>
+            Por favor verifique se esta logado com a extensão do  
+            <a href='https://chrome.google.com/webstore/detail/tronlink/ibnejdfjmmkpcnlpebklmnkoeoihofec'>
+              <strong> Tron Link</strong>
+            </a>
+          </p>
+        )}
     </Box>
   )
 }
--- src/components/LoginWallet/style.ts
+++ src/components/LoginWallet/style.ts
@@ -10,5 +10,28 @@ export const Box = styled.div`
 
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+
+    label{
+        font-size: 1.5rem;
+        font-weight: 500;
+    }
+
+    button{
+        width: 150px;
+        padding: .4rem;
+        font-weight: 600;
+        border: none;
+        border-radius: 6px;
+        cursor: pointer;
+
+        transition: 400ms;
+
+        &:hover{
+            transform: scale(1.05);
+        }
+    }
 
 `
